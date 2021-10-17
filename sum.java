7c8afde8ac9339752475a507c73f60ded5066c35
Saurabh Tripathi <looneydreamers2251@gmail.com>
2021-10-17 14:54:16 +0530

    Added messege in sum.java

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ public class sum
     public static void main(String[] args) 
     {
       int a=9,b=8;
-      System.out.println(a+b);      
+      System.out.println("the sum is  " +(a+b));      
     }
 }
