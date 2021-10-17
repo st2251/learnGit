ad010393153bbc7ee8a4e83b959e404e2579055b
Saurabh Tripathi <looneydreamers2251@gmail.com>
2021-10-17 15:12:43 +0530

    Comitted on branch saurabh/multiply

--- diff.java
+++ diff.java
@@ -3,7 +3,7 @@ public class diff
      public static void main(String[] args)
     {
         int a=10,b=7;
-        System.out.println("The diff is  " + (a+b));
+        System.out.println("The diff is  " + (a-b));
         
     }   
 }
---
+++ multiply.java
@@ -0,0 +1,8 @@
+public class multiply
+ {
+    public static void main(String[] args) 
+    {
+        int a=2,b=8;
+        System.out.println("The multiplication is " +(a*b));   
+    }    
+}
