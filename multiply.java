502d2f5ab85461899a5696c1ff04992f4659801e
tanisha-bhadani <86928104+tanisha-bhadani@users.noreply.github.com>
2021-10-18 19:27:50 +0530

    added owner's name to multiply.java

--- multiply.java
+++ multiply.java
@@ -3,6 +3,7 @@ public class multiply
     public static void main(String[] args) 
     {
         int a=2,b=8;
-        System.out.println("The multiplication is " +(a*b));   
+        System.out.println("The multiplication is " +(a*b));  
+        System.out.println("Owner Name : Saurabh Tripathi");
     }    
 }
