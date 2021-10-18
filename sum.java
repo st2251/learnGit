4ec876df52dfc3d7a8c33c8ce7dbf1f51710487e
Saurabh Tripathi <looneydreamers2251@gmail.com>
2021-10-18 17:56:39 +0530

    Gibberish added to sum.java in dev branch

--- sum.java
+++ sum.java
@@ -3,6 +3,7 @@ public class sum
     public static void main(String[] args) 
     {
       int a=9,b=8;
-      System.out.println("the sum is  " +(a+b));      
+      System.out.println("the sum is  " +(a+b)); 
+      System.out.println("jdgjsjystsytsy");     
     }
 }
